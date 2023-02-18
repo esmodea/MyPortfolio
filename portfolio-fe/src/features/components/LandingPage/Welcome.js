f0fe66fe7afb85e0da345fea14d0bde1124b5238
Esmodea Burk <esmodearburk@gmail.com>
2023-02-17 18:47:28 -0600

    Added Welcome background and image mask

--- portfolio-fe/src/features/components/LandingPage/Welcome.js
+++ portfolio-fe/src/features/components/LandingPage/Welcome.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import transparency from '../../assets/logo-transparency.png'
 
 const WelcomeTxt = () => {
     return(
-        <div>
-            <h1>Welcome!</h1>
-            <article>You have arrived at hiring paradise! <br/> This is my personal website which is fully equipped to show you my design and programming experience built from the ground up.</article>
-        </div>
+        <>
+            <div className='welcome'>
+                <div className='welcome-art'>
+                    <h1 className='welcome-title'>Welcome!</h1>
+                    <article className='article-text'>You have arrived at hiring paradise! <br/> This is my personal website which is fully equipped to show you my design and programming experience built from the ground up.</article>
+                </div>
+                <div className='article-bg'>
+                </div>
+            </div>
+        </>
     )
 }
 
