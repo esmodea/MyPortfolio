041a397ad2eac26af75fc880d5d1264fdda47ebb
Esmodea Burk <esmodearburk@gmail.com>
2023-02-19 21:42:58 -0600

    Changed naming scheme for code-content and welcome-content

--- portfolio-fe/src/features/components/AboutPage/CodeExperience.js
+++ portfolio-fe/src/features/components/AboutPage/CodeExperience.js
@@ -15,16 +15,18 @@ const CodeExperience = () => {
                 <p className="scroll-text" style={display}>scroll for more!</p>
                 <ArrowDownOutlined className="scroll-icon" />
             </div>
-            <div className="content-bg-top">
-                <div className="content-bg-bottom">
-                    <div className="content">
-                        <article className="article-text light">
-                            I have a deep understanding of Object Oriented Programming. Being extremely comfortable with JavaScript, I find I’m quite capable of following data from component to component in a React based codebase. 
-                            <br/><br/>
-                            This is an obviously important skill when it comes to things like prop drilling and redux state flow.
-                            <br/><br/>
-                            (Did I mention I have experience with Redux Toolkit?)
-                        </article>
+            <div className="code-content-bg-top">
+                <div className="code-content-bg-bottom">
+                    <div className="code-content">
+                        <div>
+                            <article className="article-text light">
+                                I have a deep understanding of Object Oriented Programming. Being extremely comfortable with JavaScript, I find I’m quite capable of following data from component to component in a React based codebase. 
+                                <br/><br/>
+                                This is an obviously important skill when it comes to things like prop drilling and redux state flow.
+                                <br/><br/>
+                                (Did I mention I have experience with Redux Toolkit?)
+                            </article>
+                        </div>
                     </div>
                 </div>
             </div>
--- portfolio-fe/src/features/components/AboutPage/Welcome.js
+++ portfolio-fe/src/features/components/AboutPage/Welcome.js
@@ -4,12 +4,12 @@ import transparency from '../../assets/logo-transparency.png'
 const WelcomeTxt = () => {
     return(
         <>
-            <div className='welcome'>
-                <div className='welcome-art'>
+            <div className='welcome-content'>
+                <div>
                     <h1 className='welcome-title'>Welcome!</h1>
                     <article className='article-text'>You have arrived at hiring paradise! <br/> <br/> This is my personal website which is fully equipped to show you my design and programming experience built from the ground up.</article>
                 </div>
-                <div className='article-bg'>
+                <div className='welcome-article-bg'>
                 </div>
             </div>
         </>
