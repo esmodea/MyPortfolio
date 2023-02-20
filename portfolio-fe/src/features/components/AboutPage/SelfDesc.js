fe739fc2a581e8fc57d8cbad441084ac59c5aee0
Esmodea Burk <esmodearburk@gmail.com>
2023-02-19 21:34:49 -0600

    Added link to Bloomtech website and stylings for said link

--- portfolio-fe/src/features/components/AboutPage/SelfDesc.js
+++ portfolio-fe/src/features/components/AboutPage/SelfDesc.js
@@ -10,7 +10,7 @@ const SelfDesc = () => {
                 <br /><br />
                 I work smart not hard, spend as little time as I can to get a problem solved right, and try to ensure I see any holes in my plans before they’re implemented if I can. I’m never averse to criticism or disagreement. If there’s a better way to do something we should do it, even if it’s not ‘the way I would’. 
                 <br /><br />
-                I’ve worked alone and in a team to deliver products through my schooling at Bloomtech Institute of Technology and have come out of it a better programmer by miles. Learning things like responsive design and RESTful API structure was a breakthrough to me. Especially as I leave Bloomtech I find myself appreciating the moments when I worked together with my team in Labs to finish a PR for the user story we implemented. Teamwork makes the dream work.
+                I’ve worked alone and in a team to deliver products through my schooling at <a href='https://www.bloomtech.com/' target={'_blank'}>Bloomtech Institute of Technology</a> and have come out of it a better programmer by miles. Learning things like responsive design and RESTful API structure was a breakthrough to me. Especially as I leave Bloomtech I find myself appreciating the moments when I worked together with my team in Labs to finish a PR for the user story we implemented. Teamwork makes the dream work.
                 </article>
                 <img src={selfPhoto} />
             </div>
