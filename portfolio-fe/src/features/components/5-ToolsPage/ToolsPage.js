b1d68b2c3f8f94d10e88281c08e740d19014dfb5
Esmodea Burk <esmodearburk@gmail.com>
2023-02-23 22:25:23 -0600

    Added foreground and background components

--- portfolio-fe/src/features/components/5-ToolsPage/ToolsPage.js
+++ portfolio-fe/src/features/components/5-ToolsPage/ToolsPage.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import NavBar from '../common/NavBar';
+import Foreground from './Foreground';
+import Background from './Background';
+import contents from './contents';
 
 const ToolsPage = () => {
     return(
         <>
             <div className='page-container' >
                 <NavBar/>
-                
+                <Foreground />;
+                <Background />;
+                <Foreground />;
             </div>
         </>
     )
