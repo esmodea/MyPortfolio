c29458dbab5daee15be1f1a72054e97a7649d3e5
Esmodea Burk <esmodearburk@gmail.com>
2023-02-23 21:42:18 -0600

    Added function to create components for reuse

--- portfolio-fe/src/features/components/4-GitPage/GitPage.js
+++ portfolio-fe/src/features/components/4-GitPage/GitPage.js
@@ -3,15 +3,21 @@ import NavBar from '../common/NavBar';
 import LeftIcon from './LeftIcon';
 import RightIcon from './RightIcon';
 import contents from './contents';
+import './gitpage.css'
 
 const GitPage = () => {
-    const [isLeft, setIsLeft] = useState();
+    let isLeft = false
     return(
         <>
             <div className='page-container' >
                 <NavBar/>
                 {contents ? contents.map((item) => {
-                    
+                    isLeft = !isLeft;
+                    if(isLeft){
+                        return <LeftIcon icon={item.icon} text={item.text} href={item.href}/>
+                    } else {
+                        return <RightIcon icon={item.icon} text={item.text} href={item.href}/>
+                    }
                 }) : 'Loading Page...'}
             </div>
         </>
