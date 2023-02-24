681bd9933b6faf45430834fd722c954ae99b1055
Esmodea Burk <esmodearburk@gmail.com>
2023-02-23 21:41:39 -0600

    changed datastructure in contents.js

--- portfolio-fe/src/features/components/4-GitPage/contents.js
+++ portfolio-fe/src/features/components/4-GitPage/contents.js
@@ -1,4 +1,8 @@
 import github from '../../assets/MyGithubIcons/github-mark.svg';
+import figma from '../../assets/MyGithubIcons/Figma-Logo.png';
+import state from '../../assets/MyGithubIcons/State.jpg';
+import rps from '../../assets/MyGithubIcons/RPS.jpg';
+import eas from '../../assets/MyGithubIcons/Etch-A-Sketch.jpg';
 
 const contents = [
     {
@@ -7,22 +11,22 @@ const contents = [
         href: 'https://github.com/esmodea/MyPortfolio'
     },
     {
-        icon: '../../assets/Figma-Logo.png',
+        icon: figma,
         text: 'On the right is a link to my Figma where you can see the effort I spent designing this site. Below here is multiple previous noteworthy sites that I’ve built in the past.',
         href: 'https://www.figma.com/file/MQHND4jGeub72LRDy3ggAg/Portfolio-Design?node-id=0%3A1&t=OvcpksRYvVM2l2pt-1'
     },
     {
-        icon: '../../assets/State.jpg',
+        icon: state,
         text: 'This is a site that I didn’t design, the link goes to a copy of the site for an example. While I did not build that copy I have coded this site from the ground up as part of my education at BloomTech.',
         href: 'https://advanced-state-wheel.herokuapp.com/'
     },
     {
-        icon: '../../assets/RPS.jpg',
+        icon: rps,
         text: 'Here is my first real JavaScript application. I keep it here because I remember it fondly.',
         href: 'https://esmodea.github.io/RockPaperScissors/'
     },
     {
-        icon: '../../assets/Etch-A-Sketch.jpg',
+        icon: eas,
         text: 'This one is a neat little Etch-A-Sketch tool that lets you choose the canvas size. It darkens each pixel by a certain amount upon mouse over. ',
         href: 'https://esmodea.github.io/Etch-A-Sketch/'
     }
