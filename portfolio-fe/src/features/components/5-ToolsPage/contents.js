f760017e0898819976b9ffe2ae8660d15290ead5
Esmodea Burk <esmodearburk@gmail.com>
2023-02-23 22:24:43 -0600

    Renamed content.js, added export: contents.

---
+++ portfolio-fe/src/features/components/5-ToolsPage/contents.js
@@ -0,0 +1,58 @@
+const contents = {
+    frontEnd: [
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+    ],
+    backEnd: [
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+    ],
+    essentials: [
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+        {
+            text: '',
+            icon: ''
+        },
+    ]
+}
+
+export default contents;
