5d1680bfaab5b0fb1f23821fab652d98fec0c154
Esmodea Burk <esmodearburk@gmail.com>
2023-02-23 23:01:16 -0600

    Used contents to populate page

--- portfolio-fe/src/features/components/5-ToolsPage/ToolsPage.js
+++ portfolio-fe/src/features/components/5-ToolsPage/ToolsPage.js
@@ -3,15 +3,16 @@ import NavBar from '../common/NavBar';
 import Foreground from './Foreground';
 import Background from './Background';
 import contents from './contents';
+import './tools.css';
 
 const ToolsPage = () => {
     return(
         <>
             <div className='page-container' >
                 <NavBar/>
-                <Foreground />;
-                <Background />;
-                <Foreground />;
+                <Foreground itemArr={contents.frontEnd} />
+                <Background itemArr={contents.backEnd} />
+                <Foreground itemArr={contents.essentials} />
             </div>
         </>
     )
