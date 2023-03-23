4352af037987409e68c544177e95e79bbcbe019b
Esmodea Burk <esmodearburk@gmail.com>
2023-03-22 19:28:26 -0500

    Removed unneeded import from Form.js

--- portfolio-fe/src/features/components/2-AbilitiesPage/Form.js
+++ portfolio-fe/src/features/components/2-AbilitiesPage/Form.js
@@ -1,6 +1,5 @@
 import React from "react";
 import IntakeForm from './IntakeForm';
-import ResumeForm from "./ResumeForm";
 import { useAuth0 } from '@auth0/auth0-react';
 
 const Form = ({Child}) => {
