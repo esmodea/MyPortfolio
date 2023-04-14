bcf906fd38dae7d7880c0468b8ea990bdb9eaad8
Esmodea Burk <esmodearburk@gmail.com>
2023-04-14 17:18:02 -0500

    Updated IntakeForm to display recieved data

--- portfolio-fe/src/features/components/2-AbilitiesPage/IntakeForm.js
+++ portfolio-fe/src/features/components/2-AbilitiesPage/IntakeForm.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import { Form, Input, Select, setFieldValue } from "antd";
-import { alterInput } from "../../state/dashboardState/userFormSlice";
+import { alterInput, updateUser } from "../../state/dashboardState/userFormSlice";
+import { getUserById } from "../../state/dashboardState/userFormAsync";
 import CustomSelect from "./CustomSelect";
 import axios from "axios";
 
@@ -9,37 +10,30 @@ const { TextArea } = Input;
 
 const IntakeForm = ({user}) => {
     const [ isRecruiter, setIsRecruiter ] = useState(false);
+    const [ userId, setUserId ] = useState()
     const state = useSelector((state) => state.userForm);
+    const userData = useSelector((state) => state.userFormAsync);
     const dispatch = useDispatch();
     const handleInputChange = (key, value) => {
         dispatch(alterInput({key: key, info: value}))
     }
     useEffect(() => {
-        (async ()=> {
-            let userID = await axios.get(`http://localhost:8000/users-email?email='${user.email}'`)
+        axios.get(`http://localhost:8000/users-email?email='${user.email}'`)
                 .then(res => {
                     console.log(res.data)
-                    userID = res.data.id;
+                    setUserId(res.data.id);
                     return res.data.id;
                 })
                 .catch(err => {
                     console.log(err)
                 })
-            if(userID){
-                (async ()=> {
-                    user = await axios.get(`http://localhost:8000/users?id=${userID.id}`)
-                        .then(res => {
-                           return res.data;
-                        })
-                        .catch(err => {
-                            console.log(err)
-                        })
-                    console.log(user)
-                })()
-                console.log('user id', userID)
-            }
-        })()
-    }, [user])
+        if(userData.firstLoad && userId )dispatch(getUserById(userId))
+        console.log(userData)
+    })
+
+    useEffect(() => {
+        if(userData.data[0])dispatch(updateUser(userData.data[0]))
+    }, [userData])
 
     const onSelectChange = (which, value) => {
         if(which === 'recruiterBool'){
@@ -55,7 +49,7 @@ const IntakeForm = ({user}) => {
         <div className="user-form-container">
             <form className="user-form">
                 <ul className="form-items">
-                    <li className="form-item">
+                    <li className="form-title">
                         <label>Please input your information!</label>
                     </li>
                     <li className="form-item username">
@@ -85,7 +79,7 @@ const IntakeForm = ({user}) => {
                             }
                         ]} />
                     </li>
-                    {state.isRecruiter ? <li className="form-item recruiting-for">
+                    {+state.isRecruiter ? <li className="form-item recruiting-for">
                         <label>Hiring for...</label>
                         <CustomSelect initialVal={state.isInterestedIn} onChange={(value) => onSelectChange('recruitingFor', value)} options={[
                             {
@@ -120,86 +114,6 @@ const IntakeForm = ({user}) => {
                     <button className="logout-button" style={{justifySelf: 'flex-end', marginLeft: 'auto'}}>Save</button>
                 </div>
             </form>
-        {/* <Form
-            className="user-form" 
-            name="intake-form"
-            size="large"
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 700 }}
-            autoComplete="off"
-        >
-            <Form.Item 
-                label="Please input your information!" 
-                name="suggestion"
-                labelCol={{ span: 24 }}
-                style={{marginLeft: '14.5rem', marginTop: '1rem'}}
-            >
-            </Form.Item>
-            <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true, message: 'You must have a username.'}]}
-                wrapperCol={{ span: 8 }}
-            >
-                <Input defaultValue={state.username} setFieldValue={state.username} />
-            </Form.Item>
-            <Form.Item
-                label="Email"
-                name='email'
-                rules={[{ required: true, message: 'You must have an email.' }]}
-                wrapperCol={{ span: 8 }}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                label="Company Name"
-                name="company"
-                rules={[{ required: false, message: 'If you work for a company please input it here.'}]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                label="Are you a recruiter?"
-                name="recruiter"
-                rules={[{ required: true, message: 'You must choose an option.'}]}
-            >
-                <Select onChange={handleRecruiter} defaultValue={'Select one please'} placeholder={'Select one please'}>
-                    <Select.Option value={'true'}>I am a recruiter</Select.Option>
-                    <Select.Option value={'false'}>I am not a recruiter</Select.Option>
-                </Select>
-            </Form.Item>
-            <Form.Item
-                noStyle
-                shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
-            >
-            {({ getFieldValue }) => getFieldValue('recruiter') === 'true' ? <Form.Item
-                label="Recruiting for"
-                name="job"
-                rules={[{ required: true, message: 'You must choose a job.'}]}
-            >
-                <Select defaultValue={''} placeholder={'Select a job'}>
-                    <Select.Option value={'full'}>FullStack Developer</Select.Option>
-                    <Select.Option value={'front'}>Front-end Developer</Select.Option>
-                    <Select.Option value={'back'}>Back-end Developer</Select.Option>
-                    <Select.Option value={'engineer'}>Software Engineer</Select.Option>
-                </Select>
-            </Form.Item> : null}
-            </Form.Item>
-            <Form.Item 
-                label="About"
-                name='about'
-                rules={[{ required: false, message: `I don't know how you managed to see this.`}]}
-            >
-                <TextArea rows={4} />
-            </Form.Item>
-            <Form.Item
-                label=' '
-                className="winvisible"
-            >
-            
-            </Form.Item>
-        </Form> */}
         <p className="explanation-para">Your info will only be used for the purposes of my networking and job search, if you wish to remove your info including your Auth0 login please do so by hitting the delete key in the bottom left corner. <br></br> <br/> If you wish to learn more about how I've built this site then please click the information icon in the top right beside the logout button.</p>
         </div>
     )
