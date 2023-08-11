7fdaeb4e0a75c948b80bd198f9cc32458a27f500
Esmodea Burk <esmodearburk@gmail.com>
2023-08-10 21:20:40 -0500

    Changed git page to mock data source.

--- portfolio-fe/src/features/components/4-GitPage/GitPage.js
+++ portfolio-fe/src/features/components/4-GitPage/GitPage.js
@@ -5,12 +5,12 @@ import Footer from '../common/Footer';
 import LeftIcon from './LeftIcon';
 import RightIcon from './RightIcon';
 import { fetchGit } from '../../state/gitState/gitSlice';
+import contents from './contents'
 import './gitpage.css';
 
 const GitPage = () => {
     const state = useSelector((state) => state.git)
     const dispatch = useDispatch();
-    const contents = state.data;
 
     useEffect(() => {
         dispatch(fetchGit())
